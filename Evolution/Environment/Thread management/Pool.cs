72363e7baf33bb03380978eadacb104ecfa97e71
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-07-02 19:06:57 +0200

    Main interface of pool

---
+++ Evolution/Environment/Thread management/Pool.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evolution
+{
+    class Pool<Creature>
+    {
+
+        public int Count { get; }
+
+        public void AddCreature(Creature creature)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Creature ExtractCreature()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExtractCreaturesTo(Creature[] container)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
