3e906a42dab0747069de18eb50246fcdba5e08ac
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-07-03 12:14:39 +0200

    StartingPool first implementation

--- Evolution/Environment/Thread management/StartPool.cs
+++ Evolution/Environment/Thread management/StartPool.cs
@@ -6,16 +6,32 @@ namespace Evolution.Environment.Thread_management
 {
     class StartPool<Creature>
     {
-        public int RemainingCratures { get; private set; }
-        public bool IsEmpty => RemainingCratures == 0;
+        public bool IsEmpty { get; private set; }
 
+        private IEnumerable<RatedCreature<Creature>> creaturesCollection;
+        private IEnumerator<RatedCreature<Creature>> enumeratorOfCreatures;
+        
         /// <summary>
         /// Return next creature and throws exception if set is empty
         /// </summary>
-        /// <returns></returns>
-        public Creature GetNextCreature()
+        public RatedCreature<Creature> GetNextCreature()
         {
-            throw new NotImplementedException();
+            if (IsEmpty)
+                throw new PoolIsEmptyException();
+
+            RatedCreature<Creature> toReturn = enumeratorOfCreatures.Current;
+            IsEmpty = !enumeratorOfCreatures.MoveNext();
+
+            return toReturn;
+        }
+
+        public void FillWithNewCreatures(IEnumerable<RatedCreature<Creature>> newCollection)
+        {
+            creaturesCollection = newCollection;
+            enumeratorOfCreatures = creaturesCollection.GetEnumerator();
+            IsEmpty = !enumeratorOfCreatures.MoveNext();
         }
     }
+
+    class PoolIsEmptyException : Exception { }
 }
