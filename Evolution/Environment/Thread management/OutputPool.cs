df18d58829a85d72a6effd7a094eb56d5e49a164
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-07-03 16:39:29 +0200

    Added class OutputPool and its interface

---
+++ Evolution/Environment/Thread management/OutputPool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evolution.Environment.Thread_management
+{
+    class OutputPool<Creature> //TODO implement OutputPool
+    {
+
+        public void SaveRatedCreature(RatedCreature<Creature> creature)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
