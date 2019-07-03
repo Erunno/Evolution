a295d75743fbc90cfaa9efe89b4ae2ffa288bd40
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-07-03 18:58:55 +0200

    OutputManager implemented

--- Evolution/Environment/Thread management/Threads/OutputManager.cs
+++ Evolution/Environment/Thread management/Threads/OutputManager.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Evolution
 {
     class OutputManager<Creature>
     {
+        public OutputPool<Creature> RatedCreatures { get; set; }
+        public EnvironmentOf<Creature> myEnvironment { get; set; }
+
+        /// <summary>
+        /// Entry point of OutputManager
+        /// </summary>
+        public void Run()
+        {
+            while (true)
+                ProcessOneCreature();
+        }
+
+        private void ProcessOneCreature()
+        {
+            RatedCreature<Creature> creatureToProcess = TryGetCreatureOrFallAsleep();
+
+            myEnvironment.Selector.AddCreature(creatureToProcess);
+        }
+
+        private RatedCreature<Creature> TryGetCreatureOrFallAsleep()
+        {
+            lock (RatedCreatures)
+            {
+                while (RatedCreatures.IsEmty)
+                    Monitor.Wait(RatedCreatures.IsNotEmptyPulser);
+
+                return RatedCreatures.GetCreature();
+            }
+        }
+
     }
 }
