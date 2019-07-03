a8eb6827b4ebf0aba1a36eab1b5ff14a645aba3e
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-07-03 18:49:15 +0200

    OutputPool implemented

--- Evolution/Environment/Thread management/Pools/OutputPool.cs
+++ Evolution/Environment/Thread management/Pools/OutputPool.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Evolution
 {
     class OutputPool<Creature> //TODO implement OutputPool
     {
 
+        Queue<RatedCreature<Creature>> creatures = new Queue<RatedCreature<Creature>>();
+
+        public Pulser IsNotEmpty = new Pulser();
+
+
         public void SaveRatedCreature(RatedCreature<Creature> creature)
         {
-            throw new NotImplementedException();
+            creatures.Enqueue(creature);
+
+            if (creatures.Count == 1)
+                lock (IsNotEmpty)
+                    Monitor.Pulse(IsNotEmpty);
         }
+
+        public RatedCreature<Creature> GetCreature() => creatures.Count != 0 ? creatures.Dequeue() : throw new PoolIsEmptyException();
     }
 }
