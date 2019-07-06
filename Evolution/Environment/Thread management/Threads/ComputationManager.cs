f58ca6884ec53b047d2e08972e24bbbcfbc3c4c4
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-07-06 21:14:18 +0200

    Computation manager - main structure implemented but i didnt solved how recognise end of computation

--- Evolution/Environment/Thread management/Threads/ComputationManager.cs
+++ Evolution/Environment/Thread management/Threads/ComputationManager.cs
@@ -9,10 +9,105 @@ namespace Evolution
     {
         private List<PairPC<Creature>> ActiveJobs;
 
+        private List<PairPC<Creature>> ActiveProducents = new List<PairPC<Creature>>();
+        private List<PairPC<Creature>> ActiveConsuments = new List<PairPC<Creature>>();
+
         private StartPool<Creature> startPool;
         private InterPool<Creature> interPool;
         private OutputPool<Creature> outputPool;
 
+        private EnvironmentOf<Creature> myEnvironment;
+
+        private OutputManager<Creature> outputManager;
+        private Thread threadOfOutputManager;
+
+        public ComputationManager(EnvironmentOf<Creature> environment)
+        {
+            myEnvironment = environment;
+
+            CreateOutputManagerAndHisThread();
+            threadOfOutputManager.Start();
+
+            CreatePools();
+
+            CreateJobs();
+            StartJobs();
+        }
+
+        private void CreateJobs()
+        {
+            ActiveJobs = new List<PairPC<Creature>>();
+            for (int i = 0; i < myEnvironment.MaximalNumOfRunningThreads; i++)
+            {
+                Producent<Creature> prod = new Producent<Creature>(myEnvironment);
+                prod.SourcePool = startPool;
+                prod.TargetPool = interPool;
+
+                ConsumentRater<Creature> cons = new ConsumentRater<Creature>(myEnvironment);
+                cons.SourcePool = interPool;
+                cons.TargetPool = outputPool;
+
+                ActiveJobs.Add(new PairPC<Creature>(prod, cons));
+            }
+        }
+
+        private void CreatePools()
+        {
+            startPool = new StartPool<Creature>();
+            interPool = new InterPool<Creature>();
+            outputPool = new OutputPool<Creature>();
+        }
+
+        private void CreateOutputManagerAndHisThread()
+        {
+            outputManager = new OutputManager<Creature>();
+            outputManager.myEnvironment = myEnvironment;
+            outputManager.RatedCreatures = outputPool;
+
+            threadOfOutputManager = new Thread(outputManager.Run);
+        }
+
+        private void StartJobs()
+        {
+            MarkAllAsProducents();
+
+            foreach (var pair in ActiveJobs)
+            {
+                pair.ConsumerThread.Start();
+                pair.ProducentThread.Start();
+            }
+        }
+
+        private void MarkAllAsProducents()
+        {
+            ActiveConsuments.Clear();
+
+            foreach (var pair in ActiveJobs)
+                ActiveProducents.Add(pair);
+        }
+
+        public void RunOneGeneration(IEnumerable<RatedCreature<Creature>> bestCreatures)
+        {
+            MarkAllAsProducents();
+            startPool.FillWithNewCreatures(bestCreatures);
+
+            lock (startPool)
+                Monitor.PulseAll(startPool); //wake up all producents
+
+            ManageProducentsAndConsuments();
+
+            WaitToTheEnd();
+        }
+
+        private void ManageProducentsAndConsuments()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void WaitToTheEnd()
+        {
+
+        }
     }
 
     class PairPC<Creature>
