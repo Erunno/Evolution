c9da7477b711ef15f162331e528c4c800cc689e3
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-07-07 21:44:54 +0200

    NewBestCretureFoundEventDelegate has different arguments

--- Evolution/Environment/Interfaces, delegates.cs
+++ Evolution/Environment/Interfaces, delegates.cs
@@ -8,5 +8,5 @@ namespace Evolution
     /// <summary>
     /// Is called when new best creature is found
     /// </summary>
-    public delegate void NewBestCretureFoundEventDelegate<Creature>(Creature newBestCreature);
+    public delegate void NewBestCretureFoundEventDelegate<Creature>(Creature newBestCreature, double FitnessValue);
 }
