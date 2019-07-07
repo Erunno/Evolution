b4472b31d4d13fa36af48230f0be9cc8b44f5c76
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-07-07 22:31:05 +0200

    New interface FitnessFunctionFactory  and default version has been implemented

---
+++ Evolution/User interfaces/FitnessFunctionFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evolution
+{
+
+    public interface IFitnessFunctionFactory<Creature>
+    {
+        /// <summary>
+        /// Creates new instance of FitnessFunctionDelegate.
+        /// </summary>
+        FitnessFunctionDelegate<Creature> CreateNewFitnessFunctionDelegate();
+    }
+
+    public delegate FitnessFunctionDelegate<Creature> FitnessFunctionProvider<Creature>();
+
+    class DefaultFitnessFunctionFactory<Creature> : IFitnessFunctionFactory<Creature>
+    {
+        public DefaultFitnessFunctionFactory(FitnessFunctionDelegate<Creature> fitnessFunction)
+        {
+            provider = () => fitnessFunction;
+        }
+
+        public DefaultFitnessFunctionFactory(FitnessFunctionProvider<Creature> provider)
+        {
+            this.provider = provider;
+        }
+
+        FitnessFunctionProvider<Creature> provider;
+
+        public FitnessFunctionDelegate<Creature> CreateNewFitnessFunctionDelegate() => provider();
+    }
+}
