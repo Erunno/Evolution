68f24db45a7693e6aa27232d106bc107678fca7b
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-07-07 15:35:32 +0200

    Computation core has been inplmented

---
+++ Evolution/Environment/Thread management/Threads/Core/ComputationCore.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Evolution
+{
+    class ComputationCore<Creature>
+    {
+        private MutationManager<Creature> mutationManager;
+
+        private EnvironmentOf<Creature> myEnvironment;
+
+        public Pulser BackToWorkPulser { get; } = new Pulser();
+        public bool IsWorking { get; private set; } = false;
+
+        public Action WorkHasBeenDoneDelegate;
+
+        public ComputationCore(EnvironmentOf<Creature> environment, StartPool<Creature> startPool, Creature foreFather)
+        {
+            mutationManager = new MutationManager<Creature>(environment, startPool, foreFather);
+        }
+
+        /// <summary>
+        /// Entry point of Computation core
+        /// </summary>
+        public void Run()
+        {
+            lock (BackToWorkPulser)
+                Monitor.Wait(BackToWorkPulser);
+
+            while(true)
+            {
+                lock(BackToWorkPulser)
+                    IsWorking = true;
+
+                ComputeOneGenaration();
+
+                lock (BackToWorkPulser)
+                {
+                    IsWorking = false;
+                    WorkHasBeenDoneDelegate?.Invoke();
+
+                    Monitor.Wait(BackToWorkPulser);
+                }
+            }
+        }
+
+        private void ComputeOneGenaration()
+        {
+            IEnumerable<Creature> newGeneration = mutationManager.GetChildren();
+
+            foreach (var child in newGeneration)
+            {
+                RatedCreature<Creature> evaluatedChild = EvaluateCreature(child);
+                AddCreatureToSelector(evaluatedChild);
+            }
+        }
+
+        private RatedCreature<Creature> EvaluateCreature(Creature creature)
+        {
+            double fitnessValue = myEnvironment.Selector.FitnessFunction(creature);
+
+            return new RatedCreature<Creature>(creature, fitnessValue);
+        }
+
+        private void AddCreatureToSelector(RatedCreature<Creature> creature)
+        {
+            lock (myEnvironment.Selector)
+                myEnvironment.Selector.AddCreature(creature);
+        }
+    }
+}
