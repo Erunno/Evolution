da63af0f5fbb60eeb235ded439bd126e1aa2f62f
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-07-07 13:30:10 +0200

    Added: ComputationCore EvalutationManager MutationManager

---
+++ Evolution/Environment/Thread management/Threads/EvaluationManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evolution.Environment.Thread_management.Threads
+{
+    class EvaluationManager<Creature>
+    {
+
+    }
+}
