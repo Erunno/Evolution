66fd46435cb20d18f41a47f4dba988989194d728
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-07-08 22:02:18 +0200

    Added new test project

---
+++ SimpleTSPSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleTSPSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
