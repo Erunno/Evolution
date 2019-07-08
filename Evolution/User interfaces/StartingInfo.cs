4d4a8449b6342ac170527f2d681f9f51b632bc7e
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-07-08 21:03:40 +0200

    New concept of creating envinronmentOf<creture> Starting info file added

---
+++ Evolution/User interfaces/StartingInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evolution
+{
+    /// <summary>
+    /// Provides EnvinronmentOf<<typeparamref name="Creature"/>> with nessesary informations
+    /// </summary>
+    /// <typeparam name="Creature"></typeparam>
+    class StartingInfo<Creature>
+    {
+
+    }
+}
