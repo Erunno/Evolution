8f1ef44a64f4d5bde49af60c2fd94c7d3c2e33ee
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-07-09 13:43:22 +0200

    Cycle class has been added and implemented

---
+++ SimpleTSPSolver/Cycle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTSPSolver
+{
+    class Cycle
+    {
+        public int[] Verticies;
+
+        public Cycle(int length)
+        {
+            Verticies = new int[length];
+        }
+    }
+}
