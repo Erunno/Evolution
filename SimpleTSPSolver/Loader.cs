c1200772cb6fa608d3a8733aa9a60a153f0a10fe
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-07-09 13:44:15 +0200

    Loader added and implemented

---
+++ SimpleTSPSolver/Loader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace SimpleTSPSolver
+{
+    class TSPLoader
+    {
+        public static TSPFitnessFunction LoadFitnessFunctionFrom(TextReader input)
+        {
+            if (!int.TryParse(input.ReadLine(), out int length))
+                throw new FormatException("First line: there is no number");
+
+            int[,] values = new int[length,length];
+
+            for (int i = 0; i < length; i++)
+            {
+                string line = input.ReadLine();
+
+                string[] numbers = line.Split();
+
+                if (numbers.Length != length)
+                    throw new FormatException($"Table line {i}: Wrong number of fields");
+
+                for (int j = 0; j < length; j++)
+                {
+                    if (!int.TryParse(numbers[j], out int value_ij))
+                        throw new FormatException($"Line {i}, Column {j} of table: Field is not a number");
+
+                    values[i, j] = value_ij;
+                }
+            }
+
+            return new TSPFitnessFunction(values);
+        }
+    }
+}
