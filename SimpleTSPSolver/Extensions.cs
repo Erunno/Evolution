b47914979e57f170bdeb7a9b9736b0c7494f9c7f
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-07-10 16:25:41 +0200

    Simple swich on arrays extension

---
+++ SimpleTSPSolver/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTSPSolver
+{
+    static class Extensions
+    {
+        public static void Swich<T>(this T[] array, int i, int j)
+        {
+            T tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+        }
+    }
+}
