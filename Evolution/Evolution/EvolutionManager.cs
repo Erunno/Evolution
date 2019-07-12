e8b7317fddbfa7f18b40fa6dd18c08ac048adea7
Matyáš Brabec <matyas.brabec@seznam.cz>
2019-07-12 13:29:33 +0200

    Added Evolution manager

---
+++ Evolution/Evolution/EvolutionManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evolution
+{
+    class EvolutionManager<Creature>
+    {
+        private EnvironmentOf<Creature> environment { get; }
+        private ComputationManager<Creature> computationManager { get; }
+
+        public EvolutionManager(EnvironmentOf<Creature> environment, ComputationManager<Creature> computationManager)
+        {
+            this.environment = environment;
+            this.computationManager = computationManager;
+        }
+
+        int CountToCatastrofy = 100;
+
+        public void RunOneStepOfEvolution()
+        {
+            if (--CountToCatastrofy == 0)
+                MakeCatastrofy();
+
+            computationManager.RunOneGeneration();
+        }
+
+        private void MakeCatastrofy()
+        {
+            
+        }
+    }
+}
